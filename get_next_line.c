834a9b919788c92dc2be8952c8b9cae993a6adda
rpunet <r.punetcorvillo@gmail.com>
2020-11-03 20:55:05 +0100

    v1.0

---
+++ get_next_line.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpunet   <rpunet  @student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/23 12:35:40 by rpunet            #+#    #+#             */
+/*   Updated: 2020/08/05 21:03:10 by rpunet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*ft_strjoin(char *s, char c)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	while (s[i])
+		i++;
+	if (!(str = (char *)malloc(i + 2)))
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = c;
+	str[i + 1] = '\0';
+	free(s);
+	return (str);
+}
+
+int			get_next_line(char **line)
+{
+	char	*buffer;
+	int		flag;
+
+	buffer = (char *)malloc(2);
+	if (!line || !(*line = (char *)malloc(1)) || !buffer)
+		return (-1);
+	*line[0] = '\0';
+	while ((flag = read(0, buffer, 1)) > 0)
+	{
+		if (buffer[0] == '\n')
+			break ;
+		*line = ft_strjoin(*line, buffer[0]);
+	}
+	free(buffer);
+	return (flag);
+}
