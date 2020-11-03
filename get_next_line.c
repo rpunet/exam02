5cd3f092df75b03bd095ab219acf0168af31f76e
rpunet <r.punetcorvillo@gmail.com>
2020-11-03 20:55:39 +0100

    v1.1

--- get_next_line.c
+++ get_next_line.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rpunet   <rpunet  @student.42.fr>          +#+  +:+       +#+        */
+/*   By: rpunet   <rpunet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/23 12:35:40 by rpunet            #+#    #+#             */
 /*   Updated: 2020/08/05 21:03:10 by rpunet           ###   ########.fr       */
